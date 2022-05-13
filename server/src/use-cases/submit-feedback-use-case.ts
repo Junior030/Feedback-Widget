eb6201f76ffb1ca0bd16796405aba6ac1720b2b1
Junior030 <juniorhenrique030@gmail.com>
2022-05-13 17:37:08 -0300

    Test Jest add

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -16,6 +16,18 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request;
 
+    if (!type) {
+      throw new Error("Type is required.");
+    }
+
+    if (!comment) {
+      throw new Error("Comment is required.");
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error("Invalid screenshot format.");
+    }
+
     await this.feedbacksRepository.create({ type, comment, screenshot });
     await this.mailAdapter.sendMail({
       subject: 'Novo feedback',
@@ -24,7 +36,7 @@ export class SubmitFeedbackUseCase {
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}`,
         `</div>`
-      ].join('\n')
+      ].join('\n') 
     })
   }
 }
